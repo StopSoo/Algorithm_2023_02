8a2f991e088c910536abfd6d046ffdc1264328d8
StopSoo <jpy1030@naver.com>
2023-11-27 01:57:37 +0900

    <feat>: 컴퓨터알고리즘과실습 과제 11주차 1번 코드 수정

--- Week11/Week11_01.cpp
+++ Week11/Week11_01.cpp
@@ -5,40 +5,55 @@
 //    출력은 방문하는 정점의 번호를 순서대로 출력하면 됩니다.
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 bool visited[100];    // 방문 배열
 
 void DFS(vector<int> *list, int startNode) {
-    vector<int> searchList;
     visited[startNode] = true;  // 시작 노드 방문 체크
-    searchList.push_back(startNode);
+    cout << startNode << " ";
 
     for (int i = 0; i < list[startNode].size(); i++) {
         int next = list[startNode][i];
-        searchList.push_back(next);
-        if (visited[next]) continue;
-        DFS(list, next);
+        if (!visited[next]) {
+            DFS(list, next);
+        }
     }
+}
 
+void BFS(vector<int> *list, int startNode) {
+    vector<int> searchList; // 방문 결과 리스트
+    queue<int> q;
+
+    visited[startNode] = true;
+    q.push(startNode);
+    searchList.push_back(startNode);
+
+    while (!q.empty()) {
+        int current = q.front();    // 큐의 맨 앞 원소 반환
+        q.pop();    // 큐의 맨 앞 원소 삭제
+
+        for (int i = 0; i < list[current].size(); i++) {
+            int next = list[current][i];
+            if (!visited[next]) {
+                visited[next] = true;
+                q.push(next);
+                searchList.push_back(next);
+            }
+        }
+    }
     // 출력
-    cout << "DFS : ";
     for (int i = 0; i < searchList.size(); i++)
         cout << searchList[i] << " ";
     cout << endl;
-}
-
-void BFS(vector<int> list, int startNode) {
-    vector<int> searchList;
-
-    
 }
 
 int main() {
     // 변수 선언과 입력
     int v, e, s;    // 정점 개수, 간선 개수, 탐색을 시작할 정점의 번호
     cin >> v >> e >> s;
-    vector<int> list[v+1];    // 인접리스트 선언 및 초기화
+    vector<int> list[v+1];    // 인접 리스트 선언 및 초기화
     for (int i = 0; i < e; i++) {
         int n1, n2;
         cin >> n1 >> n2;
@@ -46,7 +61,15 @@ int main() {
         list[n1].push_back(n2);
         list[n2].push_back(n1);
     }
+    // 함수 실행
+    fill(visited, visited + v + 1, false);
+    cout << "DFS : ";
     DFS(list, s);
+    cout << endl;
+
+    fill(visited, visited + v + 1, false);
+    cout << "BFS : ";
+    BFS(list, s);
 
     return 0;
 }
