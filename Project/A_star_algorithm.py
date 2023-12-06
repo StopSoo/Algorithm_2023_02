2c475d087875ddc9e41e51bc9678fec60a1b4a7c
StopSoo <jpy1030@naver.com>
2023-12-06 16:30:21 +0900

    A* star 알고리즘 2차 작성: 시간 내에 최대 개수 부스 돌기

--- Project/A_star_algorithm.py
+++ Project/A_star_algorithm.py
@@ -1,9 +1,28 @@
 import random
 
-# 부스 체험 시간 배열
-timeWeight = [1, 6, 2, 7, 4, 8, 9, 10, 5, 2, 3, 8, 4, 6, 9, 10, 7, 1, 8, 5]
-# 부스 방문 여부 체크 배열
-isVisited = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# 부스 이용 시간 딕셔너리 (부스 번호: 부스 이용 시간)
+timeWeight = {
+    1: 1,
+    2: 6,
+    3: 2,
+    4: 7,
+    5: 4,
+    6: 8,
+    7: 9,
+    8: 10,
+    9: 5,
+    10: 2,
+    11: 3,
+    12: 8,
+    13: 4,
+    14: 6,
+    15: 9,
+    16: 10,
+    17: 7,
+    18: 1,
+    19: 8,
+    20: 5
+}
 
 # 부스 간 이동 소요 시간
 movingWeightMatrix = [
@@ -46,50 +65,9 @@ def adjlist(matrix):
         graph[node] = neighbors
     return graph
 
+# 인접 리스트 생성
 resultAdjList = adjlist(movingWeightMatrix)
-print(resultAdjList)
-
-# 휴리스틱 함수 - 시간 내에 최대 개수의 부스 돌기
-# def heuristic(adjList, givenTime):
-#   route = []  # 최종 경로
-#   open = []
-#   close = set()
-#   totalWeight = 0  # g(n)
-#   expectedWeights = []  # h(n)
-#   # 1 ~ 사용자가 입력한 시간의 절반 중 랜덤으로 h(n) 값 설정
-#   print('# h(n) 출력 :', end=' ')
-#   for i in range(20):
-#     expectedWeights.append(random.randrange(1, int(givenTime / 2)))
-#     print(expectedWeights[i], end=' ')
-#   print()
-#   print()
-
-#   while totalWeight <= givenTime and len(close) < len(adjList):
-#     nowNodeIndex = min(range(len(timeWeight)), key=lambda i: timeWeight[i])
-#     neighbors = adjList[nowNodeIndex + 1]
-#     print('neighbors : ', neighbors)
-
-#     compareValue = {}  # g(n) + h(n) 값을 비교하기 위함
-#     for neighbor, weight in neighbors.items():
-#       if neighbor not in close:
-#         compareValue[neighbor] = weight + expectedWeights[neighbor - 1]
-#     print('compareValue : ', compareValue)
-
-#     if not compareValue:
-#       break
-
-#     nextNode = min(compareValue, key=compareValue.get)
-#     open.append(nextNode)
-#     totalWeight += timeWeight[nextNode - 1] + movingWeightMatrix[nowNodeIndex][nextNode - 1]
-#     route.append(nextNode)
-#     close.add(nextNode)
-#     isVisited[nextNode - 1] = 1
-#     print('현재 부스 \'' + str(nextNode) + '\'번을 이용 중입니다.')
-
-#   return route
-
-# route = heuristic(resultAdjList, 30)
-# print(route)
+# print(resultAdjList)
 
 # 시작 노드가 timeWeight의 최소값을 가지는 노드
 def heuristic(adjList, givenTime):
@@ -98,6 +76,7 @@ def heuristic(adjList, givenTime):
     close = set()
     totalWeight = 0  # g(n)
     expectedWeights = []  # h(n)
+    expectedWeights.append(0) # 부스 번호 맞추기 위해 리스트의 크기를 21로 설정
     # 1 ~ 사용자가 입력한 시간의 절반 중 랜덤으로 h(n) 값 설정
     print('# h(n) 출력 :', end=' ')
     for i in range(20):
@@ -106,34 +85,59 @@ def heuristic(adjList, givenTime):
     print()
     print()
 
-    # 시작 노드 설정: timeWeight 배열에서 최소값의 인덱스를 찾아서 사용
-    startNodeIndex = timeWeight.index(min(timeWeight))
-    open.append(startNodeIndex + 1)  # Adjusting index to start from 1
-    current_node = startNodeIndex + 1
+    # 시작 노드 설정: timeWeight 배열에서 최소값의 인덱스를 찾아서 사용 -> 부스 체험 시간이 가장 짧은 부스를 먼저 방문
+    startNodeIndex = list(timeWeight.keys())[list(timeWeight.values()).index(min(timeWeight))]
+    # 임시 출력
+    print('첫 부스 :', startNodeIndex)
+    print()
+    # 임시 출력
+    totalWeight += timeWeight[startNodeIndex] # 부스 이용 시간 누적
+    open.append(startNodeIndex)
+    route.append(startNodeIndex)  # 방문 부스 리스트에 첫 번쨰로 방문하는 부스 번호 추가
+    print('사용자가 첫 부스로 \'' + str(startNodeIndex) + '\'번을 방문하였습니다.')
+    print()
+    close.add(startNodeIndex) # 첫 부스 방문 종료
 
+    currentNode = timeWeight[startNodeIndex]  # 현재 노드 값 저장
+    currentNodeIndex = startNodeIndex  # 현재 노드 인덱스 초기화
     while totalWeight <= givenTime and len(close) < len(adjList):
-        neighbors = adjList[current_node]
-        print('neighbors : ', neighbors)
+        print('다음 부스로 이동합니다.')
+        print('~ ~ ~ 이동 중 ~ ~ ~')
 
         compareValue = {}  # g(n) + h(n) 값을 비교하기 위함
+        neighbors = adjList[currentNodeIndex]
+        compareValue[1] = float('inf')
         for neighbor, weight in neighbors.items():
             if neighbor not in close:
-                compareValue[neighbor] = weight + expectedWeights[neighbor - 1]
-        print('compareValue : ', compareValue)
+                compareValue[neighbor] = totalWeight + timeWeight[neighbor] + weight + expectedWeights[neighbor] # g(n) + h(n)을 계산하여 저장
+            else:
+                compareValue[neighbor] = float('inf')
+        print('g(n) + h(n):', compareValue)
 
         if not compareValue:
             break
 
-        nextNode = min(compareValue, key=compareValue.get)
-        open.append(nextNode)
-        totalWeight += timeWeight[nextNode - 1] + movingWeightMatrix[current_node - 1][nextNode - 1]
-        route.append(nextNode)
-        close.add(nextNode)
-        current_node = nextNode
-        isVisited[nextNode - 1] = 1
-        print('현재 부스 \'' + str(nextNode) + '\'번을 이용 중입니다.')
+        nextNode = min(list(compareValue.values()))  # g(n)+h(n)의 최소값 계산
+
+        print('다음 부스로 이동하기 위해 가중치를 계산 중입니다.')
+        print('g(n) + h(n)의 최소 값은', nextNode, '입니다.')
+
+        nextNodeIndex = list(timeWeight.keys())[list(compareValue.values()).index(nextNode)]
+        print('다음 부스는', nextNodeIndex, '입니다.')
+        if (totalWeight + nextNode > givenTime):  # 시간 초과 시 종료
+            print('다음 부스 이용 시 시간이 초과되어 다음 부스 이동을 보류합니다.')
+            print()
+            break
+
+        open.append(nextNodeIndex)
+        totalWeight = nextNode # (다음 부스로의 이동 시간 + 다음 부스 이용 시간) 누적 (compareValue 계산 시 포함되어 있음)
+        route.append(nextNodeIndex) # 방문 부스 리스트에 다음 부스 번호 추가
+        close.add(nextNodeIndex)  # 다음 부스 방문 종료
+        currentNodeIndex = nextNodeIndex # 현재 노드를 nextNode로 변경
+        print('현재 부스 \'' + str(currentNodeIndex) + '\'번을 이용 중입니다.')
+        print()
 
     return route
 
 route = heuristic(resultAdjList, 30)
-print(route)
+print('사용자가 입력한 시간 내 이동할 수 있는 부스 경로는', route, '입니다.')
