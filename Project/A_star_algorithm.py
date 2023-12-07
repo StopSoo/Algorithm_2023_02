d044b54a3bd0e884c1ee12535e8569d1ba21b527
StopSoo <jpy1030@naver.com>
2023-12-07 22:04:02 +0900

    A* star 알고리즘 3차 작성: 케이스 별 휴리스틱 함수 작성 및 인접리스트 값 수정

--- Project/A_star_algorithm.py
+++ Project/A_star_algorithm.py
@@ -26,26 +26,26 @@ timeWeight = {
 
 # 부스 간 이동 소요 시간
 movingWeightMatrix = [
-    [0, 4, 5, 2, 4, 3, 4, 5, 5, 3, 3, 5, 1, 2, 2, 5, 3, 2, 2, 2],
-    [4, 0, 3, 4, 3, 3, 4, 4, 5, 2, 1, 1, 5, 5, 5, 2, 2, 2, 1, 4],
-    [5, 3, 0, 3, 3, 1, 2, 1, 2, 2, 3, 4, 1, 4, 1, 3, 5, 2, 4, 5],
-    [2, 4, 3, 0, 1, 5, 4, 5, 2, 2, 2, 5, 2, 2, 4, 1, 2, 3, 2, 2],
-    [4, 3, 3, 1, 0, 2, 1, 4, 3, 4, 3, 1, 3, 3, 5, 2, 3, 3, 2, 3],
-    [3, 3, 1, 5, 2, 0, 5, 2, 5, 2, 5, 5, 2, 1, 2, 2, 3, 5, 5, 3],
-    [4, 4, 2, 4, 1, 5, 0, 1, 1, 4, 1, 3, 4, 1, 2, 2, 2, 1, 2, 3],
-    [5, 4, 1, 5, 4, 2, 1, 0, 3, 2, 2, 5, 1, 1, 5, 4, 4, 2, 3, 1],
-    [5, 5, 2, 2, 3, 5, 1, 3, 0, 4, 4, 3, 4, 1, 3, 3, 3, 3, 1, 4],
-    [3, 2, 2, 2, 4, 2, 4, 2, 4, 0, 1, 4, 3, 4, 1, 5, 1, 2, 1, 3],
-    [3, 1, 3, 2, 3, 5, 1, 2, 4, 1, 0, 3, 3, 1, 1, 3, 4, 3, 1, 5],
-    [5, 1, 4, 5, 1, 5, 3, 5, 3, 4, 3, 0, 2, 5, 5, 5, 4, 2, 1, 2],
-    [1, 5, 1, 2, 3, 2, 4, 1, 4, 3, 3, 2, 0, 4, 5, 5, 5, 2, 4, 2],
-    [2, 5, 4, 2, 3, 1, 1, 1, 1, 4, 1, 5, 4, 0, 1, 3, 1, 3, 5, 5],
-    [2, 5, 1, 4, 5, 2, 2, 5, 3, 1, 1, 5, 5, 1, 0, 3, 5, 3, 2, 1],
-    [5, 2, 3, 1, 2, 2, 2, 4, 3, 5, 3, 5, 5, 3, 3, 0, 3, 1, 3, 3],
-    [3, 2, 5, 2, 3, 3, 2, 4, 3, 1, 4, 4, 5, 1, 5, 3, 0, 3, 1, 4],
-    [2, 2, 2, 3, 3, 5, 1, 2, 3, 2, 3, 2, 2, 3, 3, 1, 3, 0, 5, 4],
-    [2, 1, 4, 2, 2, 5, 2, 3, 1, 1, 1, 1, 4, 5, 2, 3, 4, 4, 0, 2],
-    [2, 4, 5, 2, 3, 3, 3, 1, 4, 3, 5, 2, 2, 5, 1, 3, 4, 4, 2, 0]
+    [float('inf'), 4, 5, 2, 4, 3, 4, 5, 5, 3, 3, 5, 1, 2, 2, 5, 3, 2, 2, 2],
+    [4, float('inf'), 3, 4, 3, 3, 4, 4, 5, 2, 1, 1, 5, 5, 5, 2, 2, 2, 1, 4],
+    [5, 3, float('inf'), 3, 3, 1, 2, 1, 2, 2, 3, 4, 1, 4, 1, 3, 5, 2, 4, 5],
+    [2, 4, 3, float('inf'), 1, 5, 4, 5, 2, 2, 2, 5, 2, 2, 4, 1, 2, 3, 2, 2],
+    [4, 3, 3, 1, float('inf'), 2, 1, 4, 3, 4, 3, 1, 3, 3, 5, 2, 3, 3, 2, 3],
+    [3, 3, 1, 5, 2, float('inf'), 5, 2, 5, 2, 5, 5, 2, 1, 2, 2, 3, 5, 5, 3],
+    [4, 4, 2, 4, 1, 5, float('inf'), 1, 1, 4, 1, 3, 4, 1, 2, 2, 2, 1, 2, 3],
+    [5, 4, 1, 5, 4, 2, 1, float('inf'), 3, 2, 2, 5, 1, 1, 5, 4, 4, 2, 3, 1],
+    [5, 5, 2, 2, 3, 5, 1, 3, float('inf'), 4, 4, 3, 4, 1, 3, 3, 3, 3, 1, 4],
+    [3, 2, 2, 2, 4, 2, 4, 2, 4, float('inf'), 1, 4, 3, 4, 1, 5, 1, 2, 1, 3],
+    [3, 1, 3, 2, 3, 5, 1, 2, 4, 1, float('inf'), 3, 3, 1, 1, 3, 4, 3, 1, 5],
+    [5, 1, 4, 5, 1, 5, 3, 5, 3, 4, 3, float('inf'), 2, 5, 5, 5, 4, 2, 1, 2],
+    [1, 5, 1, 2, 3, 2, 4, 1, 4, 3, 3, 2, float('inf'), 4, 5, 5, 5, 2, 4, 2],
+    [2, 5, 4, 2, 3, 1, 1, 1, 1, 4, 1, 5, 4, float('inf'), 1, 3, 1, 3, 5, 5],
+    [2, 5, 1, 4, 5, 2, 2, 5, 3, 1, 1, 5, 5, 1, float('inf'), 3, 5, 3, 2, 1],
+    [5, 2, 3, 1, 2, 2, 2, 4, 3, 5, 3, 5, 5, 3, 3, float('inf'), 3, 1, 3, 3],
+    [3, 2, 5, 2, 3, 3, 2, 4, 3, 1, 4, 4, 5, 1, 5, 3, float('inf'), 3, 1, 4],
+    [2, 2, 2, 3, 3, 5, 1, 2, 3, 2, 3, 2, 2, 3, 3, 1, 3, float('inf'), 5, 4],
+    [2, 1, 4, 2, 2, 5, 2, 3, 1, 1, 1, 1, 4, 5, 2, 3, 4, 4, float('inf'), 2],
+    [2, 4, 5, 2, 3, 3, 3, 1, 4, 3, 5, 2, 2, 5, 1, 3, 4, 4, 2, float('inf')]
 ]
 movingWeightMatrix_len = len(movingWeightMatrix)
 
@@ -69,8 +69,8 @@ def adjlist(matrix):
 resultAdjList = adjlist(movingWeightMatrix)
 # print(resultAdjList)
 
-# 시작 노드가 timeWeight의 최소값을 가지는 노드
-def heuristic(adjList, givenTime):
+# 사용자가 1번을 선택했을 때 실행되는 함수
+def heuristic1(adjList, givenTime):
     route = []  # 최종 경로
     open = []
     close = set()
@@ -87,10 +87,6 @@ def heuristic(adjList, givenTime):
 
     # 시작 노드 설정: timeWeight 배열에서 최소값의 인덱스를 찾아서 사용 -> 부스 체험 시간이 가장 짧은 부스를 먼저 방문
     startNodeIndex = list(timeWeight.keys())[list(timeWeight.values()).index(min(timeWeight))]
-    # 임시 출력
-    print('첫 부스 :', startNodeIndex)
-    print()
-    # 임시 출력
     totalWeight += timeWeight[startNodeIndex] # 부스 이용 시간 누적
     open.append(startNodeIndex)
     route.append(startNodeIndex)  # 방문 부스 리스트에 첫 번쨰로 방문하는 부스 번호 추가
@@ -101,16 +97,12 @@ def heuristic(adjList, givenTime):
     currentNode = timeWeight[startNodeIndex]  # 현재 노드 값 저장
     currentNodeIndex = startNodeIndex  # 현재 노드 인덱스 초기화
     while totalWeight <= givenTime and len(close) < len(adjList):
-        print('다음 부스로 이동합니다.')
-        print('~ ~ ~ 이동 중 ~ ~ ~')
-
-        compareValue = {}  # g(n) + h(n) 값을 비교하기 위함
+        compareValue = {}  # g(n) + h(n) 값을 비교하기 위함 (매번 초기화됨)
         neighbors = adjList[currentNodeIndex]
-        compareValue[1] = float('inf')
         for neighbor, weight in neighbors.items():
             if neighbor not in close:
                 compareValue[neighbor] = totalWeight + timeWeight[neighbor] + weight + expectedWeights[neighbor] # g(n) + h(n)을 계산하여 저장
-            else:
+            else: # 이미 방문한 노드는 고려하지 않도록 설정
                 compareValue[neighbor] = float('inf')
         print('g(n) + h(n):', compareValue)
 
@@ -124,20 +116,90 @@ def heuristic(adjList, givenTime):
 
         nextNodeIndex = list(timeWeight.keys())[list(compareValue.values()).index(nextNode)]
         print('다음 부스는', nextNodeIndex, '입니다.')
+        print()
         if (totalWeight + nextNode > givenTime):  # 시간 초과 시 종료
             print('다음 부스 이용 시 시간이 초과되어 다음 부스 이동을 보류합니다.')
             print()
             break
 
         open.append(nextNodeIndex)
-        totalWeight = nextNode # (다음 부스로의 이동 시간 + 다음 부스 이용 시간) 누적 (compareValue 계산 시 포함되어 있음)
+        totalWeight += timeWeight[nextNodeIndex] + adjList[currentNodeIndex][nextNodeIndex] # (다음 부스로의 이동 시간 + 다음 부스 이용 시간) 누적 (수정했음! 확인해야 함!)
         route.append(nextNodeIndex) # 방문 부스 리스트에 다음 부스 번호 추가
         close.add(nextNodeIndex)  # 다음 부스 방문 종료
         currentNodeIndex = nextNodeIndex # 현재 노드를 nextNode로 변경
         print('현재 부스 \'' + str(currentNodeIndex) + '\'번을 이용 중입니다.')
-        print()
 
     return route
 
-route = heuristic(resultAdjList, 30)
-print('사용자가 입력한 시간 내 이동할 수 있는 부스 경로는', route, '입니다.')
+
+# 사용자가 2번을 선택했을 때 실행되는 함수
+def heuristic2(adjList):
+    route = []  # 최종 경로
+    open = []
+    close = set()
+    totalWeight = 0  # g(n)
+    expectedWeights = []  # h(n)
+    expectedWeights.append(0) # 부스 번호 맞추기 위해 리스트의 크기를 21로 설정
+    # 1~20 중 랜덤으로 h(n) 값 설정
+    print('# h(n) 출력 :', end=' ')
+    for i in range(20):
+        expectedWeights.append(random.randrange(1, 20))
+        print(expectedWeights[i], end=' ')
+    print()
+    print()
+
+    startNodeIndex = 1  # 시작 노드 설정: 1번 부스를 먼저 방문한다고 가정
+    totalWeight += timeWeight[startNodeIndex] # 부스 이용 시간 누적
+    open.append(startNodeIndex)
+    route.append(startNodeIndex)  # 방문 부스 리스트에 첫 번쨰로 방문하는 부스 번호 추가
+    print('사용자가 첫 부스로 \'' + str(startNodeIndex) + '\'번을 방문하였습니다.')
+    print()
+    close.add(startNodeIndex) # 첫 부스 방문 종료
+
+    currentNode = timeWeight[startNodeIndex]  # 현재 노드 값 저장
+    currentNodeIndex = startNodeIndex  # 현재 노드 인덱스 초기화
+    while len(close) < len(adjList):  # 모든 부스를 다 돌 때까지 반복
+        compareValue = {}  # g(n) + h(n) 값을 비교하기 위함 (매번 초기화됨)
+        neighbors = adjList[currentNodeIndex]
+        for neighbor, weight in neighbors.items():
+            if neighbor not in close:
+                compareValue[neighbor] = totalWeight + timeWeight[neighbor] + weight + expectedWeights[neighbor] # g(n) + h(n)을 계산하여 저장
+            else: # 이미 방문한 노드는 고려하지 않도록 설정
+                compareValue[neighbor] = float('inf')
+        print('g(n) + h(n):', compareValue)
+
+        if not compareValue:
+            break
+
+        nextNode = min(list(compareValue.values()))  # g(n)+h(n)의 최소값 계산
+
+        print('다음 부스로 이동하기 위해 가중치를 계산 중입니다.')
+        print('g(n) + h(n)의 최소 값은', nextNode, '입니다.')
+
+        nextNodeIndex = list(timeWeight.keys())[list(compareValue.values()).index(nextNode)]
+        print('다음 부스는', nextNodeIndex, '입니다.')
+        print()
+
+        open.append(nextNodeIndex)
+        totalWeight += timeWeight[nextNodeIndex] + adjList[currentNodeIndex][nextNodeIndex]
+        route.append(nextNodeIndex) # 방문 부스 리스트에 다음 부스 번호 추가
+        close.add(nextNodeIndex)  # 다음 부스 방문 종료
+        currentNodeIndex = nextNodeIndex # 현재 노드를 nextNode로 변경
+        print('현재 부스 \'' + str(currentNodeIndex) + '\'번을 이용 중입니다.')
+
+    return route, totalWeight
+
+
+
+
+# 사용자 입력 파트
+print('아래 두 가지 케이스 중 하나를 선택해주세요.')
+ansCase = input('1. 주어진 시간 내 최대 개수의 부스 돌기  |  2. 모든 부스를 최소 시간 안에 돌기 : ')
+if ansCase == '1':
+    ansTime = int(input('주어진 시간을 입력해주세요(단위: 분) : '))
+    route = heuristic1(resultAdjList, ansTime)
+    print('사용자가 입력한 시간 내 이동할 수 있는 부스 경로는', route, '입니다.')
+else:
+    route, ansTime = heuristic2(resultAdjList)
+    print('사용자가 최소 시간으로 모든 부스를 방문할 수 있는 부스 경로는', route, '입니다.')
+    print('최소 시간은 ' + str(ansTime) + '분입니다.')
